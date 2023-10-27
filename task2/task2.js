0880179ba2b1a9cf6904e228c77501b79e2c6684
SPACEta <tatyana4aris@mail.ru>
2023-10-27 16:34:37 +0300

    Add task2

---
+++ task2/task2.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+console.log(`1. Количество символов в тексте: ${text.length}`);
+console.log(`2. Текст с переносами строк: \n ${result}`);
+console.log(`3. Текст с удалёнными пробелами: ${substring}`);
+console.log(`4. Извлечение подстроки: ${newText}`);
+console.log(`5. Замена "клён" на "дубе": ${replacedText}`);
+console.log(`6. Строка result в верхнем регистре: ${replacedText2}`);
+console.log(`7. Замена "клён" на "дуб" (все вхождения) в переменной result: ${replacedText3}`);
+console.log(`8. Индекс первого вхождения "моря": ${index}`);
+console.log(`9. Строка с измененной первой буквой: ${modifiedText}`);
